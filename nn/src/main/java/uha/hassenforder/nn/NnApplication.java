a65bb390fb9533631cb13c8b0400817156beeb15
craftmusic <michel.stegmiller3@gmail.com>
2023-06-04 10:59:54 +0200

    Serveur python OK

--- nn/src/main/java/uha/hassenforder/nn/NnApplication.java
+++ nn/src/main/java/uha/hassenforder/nn/NnApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NnApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(NnApplication.class, args);
 	}
-
 }
